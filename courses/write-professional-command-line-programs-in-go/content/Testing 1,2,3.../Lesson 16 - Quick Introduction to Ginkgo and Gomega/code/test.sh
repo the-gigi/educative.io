0b804525835265b57ac1ed434ebaf53b6ca1da02
the-gigi <the.gigi@gmail.com>
2021-01-30 13:12:52 -0800

    update Dockerfile + test script

--- courses/write-professional-command-line-programs-in-go/content/Testing 1,2,3.../Lesson 16 - Quick Introduction to Ginkgo and Gomega/code/test.sh
+++ courses/write-professional-command-line-programs-in-go/content/Testing 1,2,3.../Lesson 16 - Quick Introduction to Ginkgo and Gomega/code/test.sh
@@ -1,4 +1,3 @@
-cd /
-git clone https://github.com/the-gigi/educative.io.git
-cd "educative.io/courses/write-professional-command-line-programs-in-go/content/Testing 1,2,3.../Lesson 16 - Quick Introduction to Ginkgo and Gomega/code"
+ln -s "/educative.io/courses/write-professional-command-line-programs-in-go/content/Testing 1,2,3.../Lesson 16 - Quick Introduction to Ginkgo and Gomega/code" /code
+cd /code
 go test
